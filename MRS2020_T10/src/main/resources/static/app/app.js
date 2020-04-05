f44b91f57dc65e68383c7517fe4a1d39dfe4ff80
teodoranedic <nedicteodora1@gmail.com>
2020-04-05 14:00:00 +0200

    dodate stranice na klijentskoj strani

--- MRS2020_T10/src/main/resources/static/app/app.js
+++ MRS2020_T10/src/main/resources/static/app/app.js
@@ -1,10 +1,17 @@
 const Empty = { template: '<empty></empty>' }
+const DodavanjeTipaPregleda = { template: '<dodtipapregleda></dodtipapregleda>' }
+const RegistracijaKlinike = { template: '<regklinike></regklinike>' }
+const StranicaZaRegistraciju = { template: '<strzareg></strzareg>' }
 
 
 const router = new VueRouter({
 	mode: 'hash',
 	  routes: [
-	    { path: '/', component: Empty}
+	    { path: '/', component: Empty},
+	    { path: '/dtpregled', component: DodavanjeTipaPregleda},
+	    { path: '/regklinika', component: RegistracijaKlinike},
+	    { path: '/registracija', component: StranicaZaRegistraciju}
+	    
 	  ]
 });
 
