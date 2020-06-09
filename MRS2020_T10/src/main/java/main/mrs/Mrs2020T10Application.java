d536b9962c8e6379928261c07c901270e67944e8
Andjela Trajkovic <trajkovicka.9909@gmail.com>
2020-06-09 14:42:26 +0200

    Pokusaj 123456

--- MRS2020_T10/src/main/java/main/mrs/Mrs2020T10Application.java
+++ MRS2020_T10/src/main/java/main/mrs/Mrs2020T10Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+  
 @EnableScheduling
 @SpringBootApplication
 @EnableAsync //ukljucena podrska za izvrsavanje asinhronih zadataka 
